082fb746c04553e52d4cc4671e4dca6c6d642ef7
Eltody <tomas.zatko.ms@gmail.com>
2021-12-11 21:48:42 +0100

    changed code

--- src/ims-project.cpp
+++ src/ims-project.cpp
@@ -1,5 +1,4 @@
 #include <simlib.h>
-#include <getopt.h>
 #include <iostream>
 #include "ims-project.hpp"
 
@@ -10,6 +9,9 @@ Facility EndYear("EndYear");
 int yearCnt = 2018;
 
 unsigned long electric_cars = DEFAULT_ELECTRIC_CARS_NUM;
+unsigned long ev_consumption = ONE_YEAR_EV_CONSUPTION;
+unsigned long year_ev_consumption = 0;
+unsigned long all_ev_consumption = 0;
 
 bool too_old_car(double type) {
     if (Random() > type) {
@@ -26,21 +28,21 @@ public:
         Priority = 2;
 
         newYearElectric:
-        
+	year_ev_consumption = electric_cars * ev_consumption;     
         for (unsigned long i = 0; i < electric_cars; i++) {
             if (too_old_car(TOO_OLD_ELECTRIC_CAR)) {
                 electric_cars--;
-
+ 
                 for (int j = 0; j < 2; j++) {
                         electric_cars++;
                 }
             } else {
-                if (this->too_old_battery()) {
-			// zvysenie energie na vyrobu novej baterie pre nove auto
-                }
+                if (this->too_old_battery()) {       
+		}
             }
         }
-
+	cout << "- year EV consumption:    " << year_ev_consumption << " kWh" << endl;
+	all_ev_consumption += year_ev_consumption; 
         Passivate();
         
         goto newYearElectric;
@@ -65,12 +67,10 @@ class YearTimer: public Event {
 
 public:
     ElectricCar* e_car = new ElectricCar;
-    Stat* CO2Stat = new Stat("Year CO2");
 
     void Behavior() {
         e_car->Activate();
 
-        
         cout << "-------------------------------------------------------------" << endl;
         cout << "---                  Year " << yearCnt++ << "                            ---" << endl;
         cout << "- Number of electric cars:    " << electric_cars << endl;
@@ -91,7 +91,7 @@ int main(){
     (new YearTimer)->Activate();  
     Run();
 
-    SIMLIB_statistics.Output();
+    cout << "- All EV consumption:    " << all_ev_consumption << " kWh" << endl;
 
     return 0;
 }
