38f851ea5aff8276e6be477d69e5c02e5debd8b4
Eltody <tomas.zatko.ms@gmail.com>
2021-12-11 22:12:10 +0100

    deleted all ev consumption, only year ev consupmtion needed

--- src/ims-project.cpp
+++ src/ims-project.cpp
@@ -9,9 +9,8 @@ Facility EndYear("EndYear");
 int yearCnt = 2018;
 
 unsigned long electric_cars = DEFAULT_ELECTRIC_CARS_NUM;
-unsigned long ev_consumption = ONE_YEAR_EV_CONSUPTION;
+unsigned long ev_consumption = ONE_YEAR_EV_CONSUMPTION;
 unsigned long year_ev_consumption = 0;
-unsigned long all_ev_consumption = 0;
 
 bool too_old_car(double type) {
     if (Random() > type) {
@@ -30,19 +29,19 @@ public:
         newYearElectric:
 	year_ev_consumption = electric_cars * ev_consumption;     
         for (unsigned long i = 0; i < electric_cars; i++) {
-            if (too_old_car(TOO_OLD_ELECTRIC_CAR)) {
+            if (too_old_car(TOO_OLD_ELECTRIC_CAR)) {	// stare auto = kupa noveho
                 electric_cars--;
  
                 for (int j = 0; j < 2; j++) {
                         electric_cars++;
                 }
             } else {
-                if (this->too_old_battery()) {       
+                if (this->too_old_battery()) {
+			year_ev_consumption += 5130; 	// vymena baterii v aute - 1kwh = 57kwh of energy, EV = 90kwh batteries, 90 * 57 = 5130kwh for manufacturing of batteries
 		}
             }
         }
 	cout << "- year EV consumption:    " << year_ev_consumption << " kWh" << endl;
-	all_ev_consumption += year_ev_consumption; 
         Passivate();
         
         goto newYearElectric;
@@ -50,7 +49,7 @@ public:
 
 
     bool too_old_battery() {
-    if (Random() < TOO_OLD_BATTERY) {
+    if (Random() < TOO_OLD_BATTERY) {	// SIMLIB funkcia Random = normalne rozdelenie, hodnoty <0,1)
         return true;
     } else {
         return false;
@@ -90,8 +89,6 @@ int main(){
     Init(SIM_START, length_od_sim);
     (new YearTimer)->Activate();  
     Run();
-
-    cout << "- All EV consumption:    " << all_ev_consumption << " kWh" << endl;
 
     return 0;
 }
